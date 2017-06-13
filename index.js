dfd08fd5002c045a3a2bae120f0df9b27a2d3c5c
Jani Eväkallio <jani.evakallio@gmail.com>
2017-06-13 22:57:47 +0100

    Use normalized channel names

--- index.js
+++ index.js
@@ -4,12 +4,13 @@ import Firebase from './Firebase';
 // which causes warnings on Android. Ignore.
 console.ignoredYellowBox = ['Setting a timer for a long period of time'];
 
+const normalizeChannelName = channel => (channel || 'default').toLowerCase();
 /**
  * Send a message
  */
 export const send = ({channel, sender, message}) => {
   return Firebase.database()
-    .ref((channel || 'default').toLowerCase())
+    .ref(normalizeChannelName(channel))
     .push({sender, message});
 };
 
@@ -17,13 +18,15 @@ export const send = ({channel, sender, message}) => {
  * Subscribe to message updates
  */
 export const subscribe = (channel, callback) => {
-  Firebase.database().ref(channel).on('value', snapshot => {
-    const data = snapshot.val();
-    const messages = [];
-    for (const key in data) {
-      messages.push({key, ...data[key]});
-    }
+  Firebase.database()
+    .ref(normalizeChannelName(channel))
+    .on('value', snapshot => {
+      const data = snapshot.val();
+      const messages = [];
+      for (const key in data) {
+        messages.push({key, ...data[key]});
+      }
 
-    callback(messages);
-  });
+      callback(messages);
+    });
 };
